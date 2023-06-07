3ec08e07ac8b52face4337f73abc94a8801e45fa
GitKing-web <ekingagada007@gmail.com>
2023-06-07 05:45:12 -0500

    updated

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,6 +1,20 @@
 const { Schema, model } = require("mongoose");
 
-const UserSchema = new Schema({}, { timestamps: true });
+const UserSchema = new Schema({
+    fullname: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
 
 const User = model("User", UserSchema);
 
