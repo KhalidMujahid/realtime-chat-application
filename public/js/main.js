10102455c102c4924c6c8644c4782267f952385a
GitKing-web <ekingagada007@gmail.com>
2023-06-26 12:47:29 -0500

    group chat

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,7 @@
-console.log("Hello JS");
+const socket = io();
+const button = document.querySelector('.toggle');
+const body = document.querySelector('body')
+button.addEventListener('click', e => {
+  e.preventDefault();
+  body.classList.toggle('dark-mode');
+});
